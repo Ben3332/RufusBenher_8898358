e744910aeb430055c07def9f981479fa08020f09
Malcom <mcbenhz2@gmail.com>
2023-06-26 18:17:26 -0400

    Files are done

--- calculator.py
+++ calculator.py
@@ -6,7 +6,7 @@ def subtract(num1, num2):
 
 def multiply(num1, num2):
     return num1 * num2
-
+#funny code
 def divide(num1, num2):
     if num2 == 0:
         raise ValueError("Cannot divide by zero")
